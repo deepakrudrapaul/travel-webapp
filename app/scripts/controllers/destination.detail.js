ffa7b9f53aba4b954e91386188ef6647e34e3beb
Deepak Paul <deep.paul@live.com>
2017-06-28 16:07:33 +0530

    destination detail place slider added

--- app/scripts/controllers/destination.detail.js
+++ app/scripts/controllers/destination.detail.js
@@ -21,6 +21,73 @@ angular.module('wanderwagon-webapp')
     };
 
 
+    $scope.placeDetails = [
+      {name : 'Name 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 5',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 6',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 7',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+
+      {name : 'Name 8',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum.',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      howToReach: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt.',
+      timings: '8am - 6pm',
+      price: '999',
+      imgUrl: 'images/blog3.jpg'},
+    
+    ];
+
      $scope.$watch('header1.isOpen', function (isOpen) {
       if (isOpen) {
         var someElement = angular.element(document.getElementById('accordion'));
