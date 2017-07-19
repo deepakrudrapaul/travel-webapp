13f06965b5f07ea0e8399ae232f32450cb4821c3
Deepak Paul <deep.paul@live.com>
2017-07-20 01:19:17 +0530

    remote api changed

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('ENV', {name:'development',endPoint:'http://192.168.43.42:8080/api/v1'})
+.constant('ENV', {name:'development',endPoint:'http://54.179.166.97:8080/api/v1'})
 
 ;
