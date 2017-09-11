b2ce3e4af870e65d543532866800c368f55414e4
Deepak Paul <deep.paul@live.com>
2017-09-11 16:43:59 +0530

    bug fixes

--- app/scripts/controllers/plan-trip.js
+++ app/scripts/controllers/plan-trip.js
@@ -103,17 +103,22 @@ angular.module('wanderwagon-webapp')
     });
 
     $scope.submitTravelPlanForm = function (form) {
-      console.log($scope.formObj);
+      
       if (auth.isLoggedIn()) {
-          remoteSvc.submitPlanMyTripForm($scope.formObj)
-            .success(function (data) {
-              console.log(data);
-              $scope.showModal("Success !", "Your query has been submitted successfully ! Will get back to you within 24 Hours.");
-            })
-            .error(function (error) {
-              console.log(error);
-              $scope.showModal("Error", error.error.message);
-            })
+        // if(form.$valid) {
+          console.log($scope.formObj);
+          // remoteSvc.submitPlanMyTripForm($scope.formObj)
+          // .success(function (data) {
+          //   console.log(data);
+          //   $scope.showModal("Success !", "Your query has been submitted successfully ! Will get back to you within 24 Hours.");
+          // })
+          // .error(function (error) {
+          //   console.log(error);
+          //   $scope.showModal("Error", error.error.message);
+          // })
+        // } else{
+        //   $scope.showModal("Error", "Please enter details correctly !");
+        // }
       } else{
         $scope.showLoginModal("Log In", "");
       }
